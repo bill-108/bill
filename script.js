1697372a08cc9875300d9740af419a12221295a3
bill <128629687+bill-108@users.noreply.github.com>
2023-05-05 20:43:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+var sum = 0; 
+function getText(){
+    var item = (document.getElementById("form").item.value);
+    var name = (document.getElementById("form").name.value);
+    var number = (document.getElementById("form").number.value);
+    var price = (document.getElementById("form").price.value);
+    var quantity = (document.getElementById("form").quantity.value);
+    let div = document.createElement("div");
+    div.className = "titles";
+    let textarea = document.createElement("textarea");
+    div.appendChild(textarea);
+    let content = document.createTextNode(item);
+    textarea.appendChild(content);
+    let textarea2 = document.createElement("textarea");
+    div.appendChild(textarea2);
+    let content2 = document.createTextNode(name);
+    textarea2.appendChild(content2);
+    let textarea3 = document.createElement("textarea");
+    div.appendChild(textarea3);
+    let content3 = document.createTextNode(number);
+    textarea3.appendChild(content3);
+    let textarea4 = document.createElement("textarea");
+    div.appendChild(textarea4);
+    let content4 = document.createTextNode(price);
+    textarea4.appendChild(content4);
+    let textarea5 = document.createElement("textarea");
+    div.appendChild(textarea5);
+    let content5 = document.createTextNode(quantity);
+    textarea5.appendChild(content5);
+    document.body.appendChild(div);
+    sum = sum + parseInt(price);
+    let sumVar = document.getElementById('sum');
+    sumVar.innerHTML = sum;
+    if(item || name || number || price || quantity === ""){
+        textarea.value="-";
+        textarea2.value="-";
+        textarea3.value="-";
+        textarea5.value="-";};
+    if(document.getElementById("form").price.value === "0"){textarea4.value = "-"}
+};
+function printPage(){window.print();};
